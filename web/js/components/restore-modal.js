fbb86aa2088475013fbb115fe3f36c206cbc5ede
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-06-09 15:45:04 -0600

    some progress

---
+++ web/js/components/restore-modal.js
@@ -0,0 +1,42 @@
+function registerRestoreModalComponent(app) {
+    return app.component("restore-modal", {
+        template: `
+<div class="modal" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Restore Database</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>Modal body text goes here.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-danger">Restore</button>
+            </div>
+        </div>
+    </div>
+</div>`,
+        data() {
+            return {
+                name: "{{name}}",
+                type: "{{type}}",
+                id: "{{id}}",
+                children: [],
+                expanded: false
+            }
+        },
+        mounted() {
+
+        },
+        methods: {
+
+        },
+        computed: {
+            expandable() {
+                return this.children.length > 0;
+            }
+        }
+    })
+}
