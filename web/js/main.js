c78dd5ece7fb0027eef5d035973ae0904007e96e
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-06-08 22:10:06 -0600

    some progress

--- web/js/main.js
+++ web/js/main.js
@@ -1,9 +1,11 @@
 async function main() {
     const app = Vue.createApp({});
+    registerBackupModalComponent(app);
     registerCreateEntityModalComponent(app);
     registerCreateTypeModalComponent(app);
     registerDeleteEntityModalComponent(app);
     registerModifyTypeModalComponent(app);
+    registerRestoreModalComponent(app);
     registerTreeNodeComponent(app);
     app.mount('#desktop');
 }
