31d6bff338852dbd1ab1c20a7ee70fddd20158a1
rqure <64617806+rqure@users.noreply.github.com>
2024-06-14 20:40:47 +0000

    some improvements

--- web/js/components/backup-modal.js
+++ web/js/components/backup-modal.js
@@ -6,20 +6,21 @@ function registerBackupModalComponent(app, context) {
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Create Database Backup</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" @click="onCancelButtonClicked"></button>
             </div>
             <div class="modal-body">
-                <p>Create a database backup.</p>
+                <button class="btn btn-primary" :disabled="isDownloadDisabled"><a :href="blobUrl" download="qmq.db">Download Backup</a></button>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-success" data-bs-dismiss="modal" @click="onBackupButtonClicked">Backup</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="onCancelButtonClicked">Cancel</button>
+                <button type="button" class="btn btn-success" @click="onBackupButtonClicked">Backup</button>
             </div>
         </div>
     </div>
 </div>`,
         data() {
             return {
+                blobUrl: "",
                 serverInteractor: context.qConfigServerInteractor
             }
         },
@@ -27,13 +28,27 @@ function registerBackupModalComponent(app, context) {
 
         },
         methods: {
+            onCancelButtonClicked() {
+                if (this.blobUrl !== "") {
+                    window.URL.revokeObjectURL(this.blobUrl);
+                }
+
+                this.blobUrl = "";
+            },
+
             async onBackupButtonClicked() {
                 const me = this;
                 const request = new proto.qmq.WebConfigCreateSnapshotRequest();
                 me.serverInteractor
                     .send(request, proto.qmq.WebConfigCreateSnapshotResponse)
                     .then(response => {
-                        qInfo(`[backup-modal::onBackupButtonClicked] Backup database response ${response}`);
+                        qInfo(`[backup-modal::onBackupButtonClicked] Backup database response ${response.getStatus()}`);
+                        if (response.getStatus() !== proto.qmq.WebConfigCreateSnapshotResponse.StatusEnum.SUCCESS) {
+                            return;
+                        }
+                        
+                        const blob = new Blob(response.getSnapshot().serializeBinary(), {type: "application/octet-stream"});
+                        me.blobUrl = window.URL.createObjectURL(blob);
                     })
                     .catch(error => {
                         qError(`[backup-modal::onBackupButtonClicked] Failed to backup database: ${error}`);
@@ -41,7 +56,9 @@ function registerBackupModalComponent(app, context) {
             }
         },
         computed: {
-            
+            isDownloadDisabled() {
+                return this.blobUrl === "";
+            }
         }
     })
 }
--- web/js/components/restore-modal.js
+++ web/js/components/restore-modal.js
@@ -6,25 +6,21 @@ function registerRestoreModalComponent(app, context) {
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Restore Database</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" @click="onCancelButtonPressed"></button>
             </div>
             <div class="modal-body">
-                <p>Modal body text goes here.</p>
+                <input class="form-control" type="file" @change="onFileUpload">
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Restore</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="onCancelButtonPressed">Cancel</button>
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal" :disabled="isRestoreDisabled" @click="onRestoreButtonPressed">Restore</button>
             </div>
         </div>
     </div>
 </div>`,
         data() {
             return {
-                name: "{{name}}",
-                type: "{{type}}",
-                id: "{{id}}",
-                children: [],
-                expanded: false,
+                snapshot: null,
                 serverInteractor: context.qConfigServerInteractor
             }
         },
@@ -32,11 +28,42 @@ function registerRestoreModalComponent(app, context) {
 
         },
         methods: {
+            onCancelButtonPressed() {
+                this.snapshot = null;
+            },
 
+            onFileUpload(event) {
+                const me = this;
+                const file = event.target.files[0];
+                if (!file) {
+                    return;
+                }
+
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    me.snapshot = proto.qmq.DatabaseSnapshot.deserializeBinary(new Uint8Array(e.target.result));
+                };
+                reader.readAsArrayBuffer(file);
+            },
+
+            async onRestoreButtonPressed() {
+                const me = this;
+                const request = new proto.qmq.WebConfigRestoreSnapshotRequest();
+                request.setSnapshot((me.snapshot));
+                me.serverInteractor
+                    .send(request, proto.qmq.WebConfigRestoreSnapshotResponse)
+                    .then(response => {
+                        qInfo(`[restore-modal::onRestoreButtonPressed] Restore database response ${response.getStatus()}`);
+                    })
+                    .catch(error => {
+                        qError(`[restore-modal::onRestoreButtonPressed] Failed to restore database: ${error}`);
+                    });
+                this.snapshot = null;
+            }
         },
         computed: {
-            expandable() {
-                return this.children.length > 0;
+            isRestoreDisabled() {
+                return this.snapshot === null;
             }
         }
     })
