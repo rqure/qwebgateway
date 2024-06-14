716f05d60aa106647172747f3a6d5707a1c601d7
rqure <64617806+rqure@users.noreply.github.com>
2024-06-14 23:04:15 +0000

    some improvements

--- web/js/components/tree-node.js
+++ web/js/components/tree-node.js
@@ -2,22 +2,58 @@ function registerTreeNodeComponent(app, context) {
     return app.component("tree-node", {
         template: `
     <li class="list-group-item list-group-item-action">
-        <div>{{name}}<span v-if="expandable">[{{ expanded ? '-' : '+' }}]</span></div>
+        <div>
+            <span class="badge text-bg-primary">{{entityType}}</span>
+            {{entityName}}
+            <span class="badge text-bg-info" v-if="expandable">{{ expanded ? '-' : '+' }}</span>
+        </div>
         <ul class="list-group">
-            <tree-node v-for="child in children" :name="child.name" :type="child.type" :id="child.id" :children="child.children"></tree-node>
+            <tree-node
+                v-for="child in entityChildren"
+                :entityName="child.entityName"
+                :entityType="child.entityType"
+                :entityId="child.entityId"
+                :entityChildren="child.entityChildren" />
         </ul>
     </li>`,
         data() {
             return {
-                name: "",
-                type: "",
-                id: "",
-                children: [],
+                entityName: "",
+                entityType: "",
+                entityId: "",
+                entityChildren: [],
                 expanded: false,
                 serverInteractor: context.qConfigServerInteractor
             }
         },
-        async mounted() {
+        async created() {
+            const getChildren = (obj, children) => {
+                children.forEach(child => {
+                    const childRequest = new proto.qmq.WebConfigGetEntityRequest();
+                    childRequest.setId(child.getId());
+
+                    this.serverInteractor
+                        .send(childRequest, proto.qmq.WebConfigGetEntityResponse)
+                        .then(childResponse => {
+                            qInfo(`[tree-node::created] Received get entity response: ${childResponse.getStatus()}`);
+                            if (childResponse.getStatus() !== proto.qmq.WebConfigGetEntityResponse.StatusEnum.SUCCESS) {
+                                return;
+                            }
+
+                            const childObj = {
+                                entityId: childResponse.getEntity().getId(),
+                                entityName: childResponse.getEntity().getName(),
+                                entityType: childResponse.getEntity().getType(),
+                                entityChildren: []
+                            };
+                            
+                            getChildren(childObj, childResponse.getEntity().getChildrenList())
+
+                            obj.entityChildren.push(childObj);
+                        });
+                });
+            };
+
             const getEntity = (entityId) => {
                 const request = new proto.qmq.WebConfigGetEntityRequest();
                 request.setId(entityId);
@@ -29,24 +65,25 @@ function registerTreeNodeComponent(app, context) {
                             return;
                         }
 
-                        this.name = response.getEntity().getName();
-                        this.type = response.getEntity().getType();
-                        this.id = response.getEntity().getId();
-                        this.children = response.getEntity().getChildrenList();
+                        this.entityName = response.getEntity().getName();
+                        this.entityType = response.getEntity().getType();
+                        this.entityId = response.getEntity().getId();
+
+                        getChildren(this, response.getEntity().getChildrenList());
                     })
                     .catch(error => {
                         qError(`[tree-node::mounted] Failed to get entity: ${error}`)
-                        this.name = "";
-                        this.type = "";
-                        this.id = "";
-                        this.children = [];
+                        this.entityName = "";
+                        this.entityType = "";
+                        this.entityId = "";
+                        this.entityChildren = [];
 
                         if (error.message === "Connection closed" ) {
                             qInfo("[tree-node::mounted] Retrying get entity request...")
                             setTimeout(() => getEntity(entityId), 1000);
                         }
                     });
-            }
+            };
 
             const getRoot = () => {
                 this.serverInteractor
@@ -60,22 +97,20 @@ function registerTreeNodeComponent(app, context) {
                     })
                     .catch(error => {
                         qError(`[tree-node::mounted] Failed to get root: ${error}`)
-                        this.name = "";
-                        this.type = "";
-                        this.id = "";
-                        this.children = [];
+                        this.entityName = "";
+                        this.entityType = "";
+                        this.entityId = "";
+                        this.entityChildren = [];
 
                         if (error.message === "Connection closed" ) {
                             qInfo("[tree-node::mounted] Retrying get root request...")
                             setTimeout(getRoot, 1000);
                         }
                     });
-            }
+            };
 
-            if (this.id === "") {
+            if (this.entityId === "") {
                 getRoot();
-            } else {
-                getEntity(this.id);
             }
         },
         methods: {
@@ -83,7 +118,7 @@ function registerTreeNodeComponent(app, context) {
         },
         computed: {
             expandable() {
-                return this.children.length > 0;
+                return this.entityChildren.length > 0;
             }
         }
     })
