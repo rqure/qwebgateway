fe4b7ef5c3d0b7785e2142bd9b1fadcfa0377ed5
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-07-18 23:09:34 -0600

    some progress

---
+++ backup.sh
@@ -0,0 +1,1 @@
+curl localhost:20000/api -d '{"header":{"id":"ea9e4bfb-a208-419e-88cc-8dc694b287ef","timestamp":"2024-07-04T22:37:18.544393318Z"},"payload":{"@type":"type.googleapis.com/qdb.WebConfigCreateSnapshotRequest"}}' | jq '.payload |= (del(.status) | .["@type"] = "type.googleapis.com/qdb.WebConfigRestoreSnapshotRequest")' > snapshot.json
